9936896c80a6b7b6c41b9c39439d562e0b693f2c
Daniel Massey <danielmassey1@gmail.com>
2018-09-17 22:35:59 +0000

    Added config sample

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -23,16 +23,16 @@
 define('DB_NAME', 'database_name_here');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'database_username_here');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'database_password_here');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'database_host_here');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define('DB_CHARSET', 'utf8mb4');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
@@ -46,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+#define('AUTH_KEY',         'put your unique phrase here');
+#define('SECURE_AUTH_KEY',  'put your unique phrase here');
+#define('LOGGED_IN_KEY',    'put your unique phrase here');
+#define('NONCE_KEY',        'put your unique phrase here');
+#define('AUTH_SALT',        'put your unique phrase here');
+#define('SECURE_AUTH_SALT', 'put your unique phrase here');
+#define('LOGGED_IN_SALT',   'put your unique phrase here');
+#define('NONCE_SALT',       'put your unique phrase here');
 
 /**#@-*/
 
